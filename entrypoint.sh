7e95f8c9445fdf1e90e547eea44fd9914f44b799
Walter Tapia <tapiaw38@gmail.com>
2025-05-02 13:21:35 -0300

    Fix configure docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-until pg_isready --host=cardon-postgres-db --port=5432 --username=postgres --dbname=auth-api-db
-do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 2
-done
-
 echo "========== Starting Go application =========="
 exec go run ./cmd/api/ --host 0.0.0.0
