34e6f509f23c43d5cacf7c696b875b262aa30570
Walter Tapia <tapiaw38@gmail.com>
2025-05-02 23:53:19 -0300

    Feat user me endpoint

---
+++ internal/adapters/web/handlers/user/me.go
@@ -0,0 +1,27 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tapiaw38/auth-api-be/internal/usecases/user"
+)
+
+func NewMeHandler(usecase user.GetUsecase) func(c *gin.Context) {
+	return func(c *gin.Context) {
+
+		username := c.GetString("userID")
+
+		userOutput, err := usecase.Execute(c, user.GetFilterOptions{
+			Username: username,
+		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, userOutput)
+	}
+}
--- internal/adapters/web/routes.go
+++ internal/adapters/web/routes.go
@@ -17,11 +17,11 @@ func RegisterApplicationRoutes(app *gin.Engine, useCases *usecases.Usecases) {
 		})
 	})
 
-	routeGroup.POST("role/ensure", role.NewEnsureHandler(useCases.Role.EnsureUsecase))
 	routeGroup.POST("auth/register", user.NewRegisterHandler(useCases.User.RegisterUsecase))
 	routeGroup.POST("auth/login", user.NewLoginHandler(useCases.User.LoginUsecase))
+	routeGroup.POST("role/ensure", role.NewEnsureHandler(useCases.Role.EnsureUsecase))
+	routeGroup.GET("role/list", role.NewListHandler(useCases.Role.ListUsecase))
 
 	routeGroup.Use(middlewares.AuthorizationMiddleware(useCases.User.GetTokenVersionUsecase))
-
-	routeGroup.GET("role/list", role.NewListHandler(useCases.Role.ListUsecase))
+	routeGroup.GET("user/me", user.NewMeHandler(useCases.User.GetUsecase))
 }
