28787ad1aa2b213007dfd9dd5e0e715612b0b537
Walter Tapia <tapiaw38@gmail.com>
2025-08-28 01:03:22 -0300

    Fix remove comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-# Wait for Postgres to be ready
-echo "⏳ Waiting for Postgres at ${PG_DB_HOST:-auth-postgres-db}:${PG_DB_PORT:-5432}..."
+echo "Waiting for Postgres at ${PG_DB_HOST:-auth-postgres-db}:${PG_DB_PORT:-5432}..."
 until nc -z "${PG_DB_HOST:-auth-postgres-db}" "${PG_DB_PORT:-5432}"; do
     sleep 1
 done
-echo "✅ Postgres is ready."
+echo "Postgres is ready."
 
-# Wait for RabbitMQ to be ready
-echo "⏳ Waiting for RabbitMQ at ${RABBIT_HOST:-rabbitmq}:${RABBIT_PORT:-5672}..."
+echo "Waiting for RabbitMQ at ${RABBIT_HOST:-rabbitmq}:${RABBIT_PORT:-5672}..."
 until nc -z "${RABBIT_HOST:-rabbitmq}" "${RABBIT_PORT:-5672}"; do
     sleep 1
 done
-echo "✅ RabbitMQ is ready."
+echo "RabbitMQ is ready."
 
 echo "🚀 Starting Go application..."
 exec go run ./cmd/api/ --host 0.0.0.0 --port 8082
